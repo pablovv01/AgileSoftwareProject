e8bcec963ce3cd33190d035af9b25f25bd2e6e4b
Pablo Varela Vázquez <pablo.vv01@outlook.es>
2024-11-06 00:03:19 +0100

    Preliminary version of register

--- src/app/presentation/register/register.component.ts
+++ src/app/presentation/register/register.component.ts
@@ -6,6 +6,10 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 import { MatCardModule } from '@angular/material/card';
 import { MatFormField, MatLabel, MatError } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
 
 
 
@@ -19,6 +23,10 @@ import { MatFormField, MatLabel, MatError } from '@angular/material/form-field';
     MatLabel,
     MatError,
     ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatSnackBarModule
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
@@ -27,8 +35,10 @@ import { MatFormField, MatLabel, MatError } from '@angular/material/form-field';
 export class RegisterComponent {
   registroForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private snackBar: MatSnackBar) {
     this.registroForm = this.fb.group({
+      name: ["", [Validators.required]],
+      type:["", [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]]
     });
@@ -40,12 +50,20 @@ export class RegisterComponent {
       const { email, password } = this.registroForm.value;
       createUserWithEmailAndPassword(auth, email, password)
         .then(userCredential => {
-          console.log("Usuario registrado:", userCredential);
-          // Aquí puedes redirigir o mostrar un mensaje de éxito
+          console.log("Usuario registered:", userCredential);
+          this.snackBar.open('User registered successfully', 'Cerrar', {
+            duration: 3000, 
+            verticalPosition: 'bottom', 
+            horizontalPosition: 'center'
+          });
         })
         .catch(error => {
           console.error("Error en el registro:", error);
-          // Puedes manejar el error mostrando un mensaje al usuario
+          this.snackBar.open('There has been an error: '+error, 'Cerrar', {
+            duration: 3000, 
+            verticalPosition: 'bottom', 
+            horizontalPosition: 'center'
+          });
         });
     }
   }
