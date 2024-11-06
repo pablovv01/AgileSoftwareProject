7cc8e691d5e76347bf6f3e792af775355256054d
Claudio Catalano <claudio.v.catalano@icloud.com>
2024-11-06 23:54:37 +0100

    Fixed form submission errors

--- src/app/presentation/add-idea/add-idea.component.ts
+++ src/app/presentation/add-idea/add-idea.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';  // Import FormsModule here
 
 @Component({
   selector: 'app-add-idea',
-  standalone: true,
-  imports: [],
+  standalone: true,  // Make it a standalone component
+  imports: [FormsModule],  // Import FormsModule for template-driven forms
   templateUrl: './add-idea.component.html',
-  styleUrl: './add-idea.component.css'
+  styleUrls: ['./add-idea.component.css']
 })
 export class addIdeaComponent {
 
+  onSubmit(form: any) {
+    console.log('Form Submitted!', form.value);
+
+    // Connect to the backend here and send the form data to the database.
+    
+  }
 }
