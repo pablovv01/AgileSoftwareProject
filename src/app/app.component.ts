4123397858ba5ff41a5ffabc2083a98c15101397
Pablo Varela Vázquez <pablo.vv01@outlook.es>
2024-11-06 14:27:33 +0100

    Change the route of app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from './presentation/core/footer/footer.component';
+import { FooterComponent } from './presentation/common/footer/footer.component';
 
 @Component({
   selector: 'app-root',
