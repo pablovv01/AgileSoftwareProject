d9df0ff3f4b542011c01953912975a56a183acf1
Pablo Varela Vázquez <pablo.vv01@outlook.es>
2024-11-06 12:06:44 +0100

    Added login route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './presentation/register/register.component';
+import { LoginComponent } from './presentation/login/login.component';
 
 export const routes: Routes = [
-    { path: 'register', component: RegisterComponent }, // Ruta para el componente de registro
-    { path: '', redirectTo: '/', pathMatch: 'full' }, // Ruta por defecto
+    { path: 'register', component: RegisterComponent }, // Route for register component
+    {path: 'login', component: LoginComponent}, //Route for login component
+    { path: '', redirectTo: '/login', pathMatch: 'full' }, // Default route
 ]
